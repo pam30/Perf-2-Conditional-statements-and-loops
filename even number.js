bfdb960df47439179e95ed9265bc7a9da85ee64b
pam30 <202375371+pam30@users.noreply.github.com>
2025-07-11 21:26:52 +0800

    Create even number.js

---
+++ even number.js
@@ -0,0 +1,21 @@
+// evenNumbers.js
+// Print numbers from 1 to 10
+console.log("All numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Print only even numbers from 1 to 10
+console.log("Even numbers from 1 to 10:");
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+/*
+Reference:
+JavaScript For Loop and Modulo Operator
+- Mozilla Developer Network (MDN): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+- W3Schools JavaScript For Loop: https://www.w3schools.com/js/js_loop_for.asp
+*/
